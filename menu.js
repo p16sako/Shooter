003105f29186f63ac3d272fdf5d97ed9d16489e9
Christos Sakoumpentas <32750300+p16sako@users.noreply.github.com>
2018-12-19 19:51:43 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,30 @@
+var menu = {
+    preload: () => {
+        game.load.image('Menu', 'assets/Menu.png');
+        game.load.image('Level1', 'assets/Level1.png');
+        game.load.image('Level2', 'assets/Level2.png');
+    },
+
+    create: () => {
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        // Picture for menu
+        var menuPic = game.add.sprite(0, 0, 'Menu');
+
+        // Picture and button to choose level 1
+        var click1 = game.add.button(game.width /2 - 100, game.height / 2 + 25, 'Level1', function(){
+            game.state.start('Level1');
+
+        });
+        click1.anchor.set(0.5,0.5);
+        
+        // Picture and button to choose level 2
+        var click2 = game.add.button(game.width /2 + 100, game.height / 2 + 25, 'Level2', function(){
+            game.state.start('Level2');
+
+        });
+        click2.anchor.set(0.5,0.5);
+    }
+}
