c762ae7313ee3f2f86581f314a5282944a7d6b09
Christos Sakoumpentas <32750300+p16sako@users.noreply.github.com>
2018-12-21 03:09:08 +0200

    Update Level2.js

--- Level2.js
+++ Level2.js
@@ -2,11 +2,6 @@ var level2 = {
 
     preload: function() {
 
-        //  We need this because the assets are on github pages
-        //  Remove the next 2 lines if running locally
-        //game.load.baseURL = 'https://ioniodi.github.io/Shooter/';
-        //game.load.crossOrigin = 'anonymous';
-
         //basic
         game.load.image('starfield', 'assets/starfield.png');
         game.load.image('ship', 'assets/ship.png');
